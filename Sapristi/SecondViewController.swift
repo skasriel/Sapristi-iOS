4f5ce1aa36e2f7ad1024e1ef67274132f87f186c
Stephane Kasriel <skasriel@elance-odesk.com>
2014-09-12 08:34:55 -0700

    Initial Commit

---
+++ Sapristi/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Sapristi
+//
+//  Created by Stephane Kasriel on 9/12/14.
+//  Copyright (c) 2014 Stephane Kasriel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
