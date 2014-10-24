273b1a454459b43f121bfbd4b0f93dcedfd7c3e7
Stephane Kasriel <skasriel@elance-odesk.com>
2014-10-23 23:27:26 -0700

    small changes

--- CarMotionViewController.swift
+++ CarMotionViewController.swift
@@ -35,16 +35,4 @@ class CarMotionViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
